b6054ae288c549026ed0772de8b78bcd90f0f43e
Conor Driscoll <cdriscoll66@gmail.com>
2023-01-16 10:57:13 -0500

    footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,11 +1,27 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-I'm a footer
+  <footer>
+    <div>© 2023 - Driscoll66.com</div>
+  </footer>
 </template>
 
 <style scoped lang="scss">
-
+footer {
+  z-index: 1;
+  position: absolute;
+  bottom: 16px;
+  width: 100%;
+  font-family: "Henderson Slab", serif;
+  text-transform: uppercase;
+  font-size: 0.5rem;
+  color: var(--color-sailor-boy);
+  padding-bottom: 0;
+  div {
+    margin: 0 auto;
+    max-width: var(--site-container-width);
+    width: 100%;
+    margin-bottom: 0;
+  }
+}
 </style>
