f1122e5011cbcd8ac9f241359cef11187f8133cb
Conor Driscoll <cdriscoll66@gmail.com>
2023-01-23 10:47:53 -0500

    undo revert

--- components/EmailForm.vue
+++ components/EmailForm.vue
@@ -1,38 +1,5 @@
 <script setup lang="ts">
 
-// interface FormShape {
-//   form: string;
-//   name: string | undefined;
-//   email: string | undefined;
-//   message: string | undefined;
-// }
-
-// const Form : FormShape = reactive({
-//     form: '#contact',
-//     name: '',
-//     email: '',
-//     message: ''
-//     });
-
-// const handleSubmit = () => {
-//     console.log('submitting form');
-// const formData:any = new FormData();
-// formData.append('name', Form.name);
-// // formData.append('email', Form.email);
-// // formData.append('message', Form.message);
-
- 
-//     fetch("/", {
-//     method: "POST",
-//     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-//     body: new URLSearchParams(formData).toString(),
-//   })
-//     .then(() => console.log("Form successfully submitted"))
-//     .catch((error) => alert(error));
-
-// }
-
-
 </script>
 
 <template>
@@ -45,90 +12,29 @@
     <input type="submit" value="Submit" />
   </form>
 
-
-<!-- 
-<form id="contact" name="contact" data-netlify="true">
+<form name="contact" method="POST" data-netlify-honeypot="bot-field" data-netlify="true">
     <input type="hidden" value="contact" name="contact" />
-    <Transition appear class="delay-16">
   <p>
-    <label for="name">Name </label>
-    <input id="name" type="text" name="name" placeholder="Name" v-model="Form.name"/>
+    <label>Your Name: <input type="text" name="name" /></label>
   </p>
-</Transition>
-<Transition appear class="delay-17">
-
   <p>
-    <label for="email">Email </label>
-    <input id="email" type="text" name="email" placeholder="Email"/>
+    <label>Your Email: <input type="email" name="email" /></label>
   </p>
-</Transition>
-  <Transition appear class="delay-18">
-
   <p>
-    <label for="message">Message </label>
-    <textarea id="message" name="message" placeholder="Drop me a note"></textarea>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
   </p>
-</Transition>
-<Transition appear class="delay-19">
-
   <p>
-    <input type="submit" value="Submit" @click.prevent="handleSubmit">
+    <label>Message: <textarea name="message"></textarea></label>
   </p>
-  </Transition>
-</form> -->
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
 </template>
 
 <style scoped lang="scss">
 
-
-label {
-    display:none;
-}
-
-form {
-    padding: 0;
-}
-
-input[type='text'], textarea {
-    background-color: var(--color-trapped-darkness);
-    border: 3px solid var(--color-maniac-mansion);
-    color: var(--color-old-lace);
-    font-family: 'Henderson Slab', serif;
-    font-size: 1.2rem;
-    padding: 10px;
-    width: 100%;
-    
-}
-
-textarea {
-  resize: none;
-}
-
-.v-enter-active {
-  transition: opacity 0.5s ease, transform 0.5s ease;
-}
-.v-enter-from {
-  opacity: 0;
-  transform: translateY(100px);
-}
-
-button
-
-
-
-.delay-16 {
-  transition-delay: 1.6s;
-}
-
-.delay-17 {
-  transition-delay: 1.7s;
-}
-
-.delay-18 {
-  transition-delay: 1.8s;
-}
-
-.delay-19 {
-  transition-delay: 1.9s;
-}
 </style>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,26 +1,3 @@
-<script setup>
-
-const handleSubmit = () => {
-    console.log('submitting form');
-const formData = new FormData();
-formData.append = ('form-name', 'contact-form'),
-formData.append('name', 'it worked');
-// formData.append('email', Form.email);
-// formData.append('message', Form.message);
-
- 
-    fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => console.log("Form successfully submitted"))
-    .catch((error) => alert(error));
-
-}
-
-</script>
-
 <template>
   <section>
     <div class="feedback-form">
@@ -49,7 +26,7 @@ formData.append('name', 'it worked');
             <textarea name="message" required placeholder="Message"></textarea>
           </div>
           <div>
-            <button type="submit" value="Send message" @click.prevent="handleSubmit">Submit</button>
+            <button type="submit" value="Send message">Submit</button>
           </div>
         </div>
       </form>
