b896d940fec10f93ca070dfdf1cc56474ce80453
Conor Driscoll <cdriscoll66@gmail.com>
2023-02-22 14:20:57 -0500

    simple sitemap gen

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    // target: "static",
-    css: ["~/assets/main.css"],
-    modules: [
-      // ...
-      [
-        "@pinia/nuxt",
-        {
-          autoImports: [
-            // automatically imports `defineStore`
-            "defineStore", // import { defineStore } from 'pinia'
-            // automatically imports `defineStore` as `definePiniaStore`
-            ["defineStore", "definePiniaStore"], // import { defineStore as definePiniaStore } from 'pinia'
-          ],
-        },
-      ],
+  // target: "static",
+  css: ["~/assets/main.css"],
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: ["/", "/work"],
+    },
+  },
+  modules: [
+    // ...
+    [
+      "@pinia/nuxt",
+      {
+        autoImports: [
+          // automatically imports `defineStore`
+          "defineStore", // import { defineStore } from 'pinia'
+          // automatically imports `defineStore` as `definePiniaStore`
+          ["defineStore", "definePiniaStore"], // import { defineStore as definePiniaStore } from 'pinia'
+        ],
+      },
     ],
-  });
+    "nuxt-simple-sitemap",
+  ],
+  runtimeConfig: {
+    public: {
+      siteUrl: process.env.NUXT_PUBLIC_SITE_URL || "https://www.driscoll66.com",
+    },
+  }
+});
